200305833f53c02c9e35ae4065d4f0b137921ab9
hz <13457698+hozzjss@users.noreply.github.com>
2023-12-23 00:02:28 +0200

    add expires at to orders table

--- apps/brc-20-api/src/util/orders/isOrderExpired.ts
+++ apps/brc-20-api/src/util/orders/isOrderExpired.ts
@@ -1,6 +1,5 @@
 import { Order } from "@prisma/client"
-import { ORDER_EXPIRATION_TIME } from "../../constants.js"
 
 export function isOrderExpired(order: Order) {
-  return order.createdAt.getTime() + ORDER_EXPIRATION_TIME < Date.now()
+  return order.expiresAt.getTime() < Date.now()
 }
