ec7579f8e7b5e46f661ab4aa48add219abc0b22f
hz <13457698+hozzjss@users.noreply.github.com>
2023-12-23 18:16:30 +0200

    update generated api client

--- apps/brc-20-frontend/src/api/generated.ts
+++ apps/brc-20-frontend/src/api/generated.ts
@@ -1,96 +1,87 @@
-type GetInput = {};
+type GetInput = {}
 
-type GetResponse =
-  | {
-      status: "success";
-      data: {
+type GetResponse = {
+    status: "success";
+    data: {
         message: string;
-      };
-    }
-  | {
-      status: "error";
-      error: {
-        message: string;
-      };
     };
+} | {
+    status: "error";
+    error: {
+        message: string;
+    };
+}
 
 type GetCheckClaimInput = {
-  address: string;
-};
+    address: string;
+}
 
-type GetCheckClaimResponse =
-  | {
-      status: "success";
-      data: {
+type GetCheckClaimResponse = {
+    status: "success";
+    data: {
         freeAmount: number;
         isWhitelistOpen: boolean;
         price: number;
         isWhitelisted: boolean;
-      };
-    }
-  | {
-      status: "error";
-      error: {
-        message: string;
-      };
     };
+} | {
+    status: "error";
+    error: {
+        message: string;
+    };
+}
 
 type GetPriceInput = {
-  amount: string;
-  feeRate: string;
-  address: string;
-};
+    amount: string;
+    feeRate: string;
+    address: string;
+}
 
-type GetPriceResponse =
-  | {
-      status: "success";
-      data: {
+type GetPriceResponse = {
+    status: "success";
+    data: {
         brc20Price: number;
         minerFees: number;
         basePostage: number;
         total: number;
-      };
-    }
-  | {
-      status: "error";
-      error: {
-        message: string;
-      };
     };
+} | {
+    status: "error";
+    error: {
+        message: string;
+    };
+}
 
 type PostOrdersInput = {} & {
-  receiveAddress: string;
-  amount: number;
-  feeRate: number;
-};
+    receiveAddress: string;
+    amount: number;
+    feeRate: number;
+}
 
-type PostOrdersResponse =
-  | {
-      status: "success";
-      data: {
+type PostOrdersResponse = {
+    status: "success";
+    data: {
         id: number;
         paymentAddress: string;
         receiveAddress: string;
         createdAt: string;
         updatedAt: string;
         totalPrice: number;
-      };
-    }
-  | {
-      status: "error";
-      error: {
-        message: string;
-      };
     };
+} | {
+    status: "error";
+    error: {
+        message: string;
+    };
+}
 
 type GetOrdersIdInput = {
-  id: string;
-};
+    id: string;
+}
 
-type GetOrdersIdResponse =
-  | {
-      status: "success";
-      data: {
+type GetOrdersIdResponse = {
+    status: "success";
+    data: {
         id: number;
         status: "UNPAID" | "EXPIRED" | "COMPLETE";
         amount: number;
@@ -101,104 +92,61 @@ type GetOrdersIdResponse =
         paymentTxId: string | null;
         transferTxId: string | null;
         receiveAddress: string;
-      };
-    }
-  | {
-      status: "error";
-      error: {
-        message: string;
-      };
     };
+} | {
+    status: "error";
+    error: {
+        message: string;
+    };
+}
 
-type GetProgressInput = {};
+type GetProgressInput = {}
 
-type GetProgressResponse =
-  | {
-      status: "success";
-      data: {
+type GetProgressResponse = {
+    status: "success";
+    data: {
         progress: number;
-      };
-    }
-  | {
-      status: "error";
-      error: {
-        message: string;
-      };
     };
+} | {
+    status: "error";
+    error: {
+        message: string;
+    };
+}
 
-export type Path =
-  | ""
-  | "/check-claim"
-  | "/price"
-  | "/orders"
-  | "/orders/:id"
-  | "/progress";
+export type Path = "" | "/check-claim" | "/price" | "/orders" | "/orders/:id" | "/progress"
 
-export type Method = "get" | "post" | "put" | "delete" | "patch";
+export type Method = "get" | "post" | "put" | "delete" | "patch"
 
-export type MethodPath = `${Method} ${Path}`;
+export type MethodPath = `${Method} ${Path}`
 
 export interface Input extends Record<MethodPath, any> {
-  "get ": GetInput;
-  "get /check-claim": GetCheckClaimInput;
-  "get /price": GetPriceInput;
-  "post /orders": PostOrdersInput;
-  "get /orders/:id": GetOrdersIdInput;
-  "get /progress": GetProgressInput;
+    "get ": GetInput;
+    "get /check-claim": GetCheckClaimInput;
+    "get /price": GetPriceInput;
+    "post /orders": PostOrdersInput;
+    "get /orders/:id": GetOrdersIdInput;
+    "get /progress": GetProgressInput;
 }
 
 export interface Response extends Record<MethodPath, any> {
-  "get ": GetResponse;
-  "get /check-claim": GetCheckClaimResponse;
-  "get /price": GetPriceResponse;
-  "post /orders": PostOrdersResponse;
-  "get /orders/:id": GetOrdersIdResponse;
-  "get /progress": GetProgressResponse;
+    "get ": GetResponse;
+    "get /check-claim": GetCheckClaimResponse;
+    "get /price": GetPriceResponse;
+    "post /orders": PostOrdersResponse;
+    "get /orders/:id": GetOrdersIdResponse;
+    "get /progress": GetProgressResponse;
 }
 
-export const jsonEndpoints = {
-  "get ": true,
-  "get /check-claim": true,
-  "get /price": true,
-  "post /orders": true,
-  "get /orders/:id": true,
-  "get /progress": true,
-};
+export const jsonEndpoints = { "get ": true, "get /check-claim": true, "get /price": true, "post /orders": true, "get /orders/:id": true, "get /progress": true }
 
-export const endpointTags = {
-  "get ": [],
-  "get /check-claim": [],
-  "get /price": [],
-  "post /orders": [],
-  "get /orders/:id": [],
-  "get /progress": [],
-};
+export const endpointTags = { "get ": [], "get /check-claim": [], "get /price": [], "post /orders": [], "get /orders/:id": [], "get /progress": [] }
 
-export type Provider = <M extends Method, P extends Path>(
-  method: M,
-  path: P,
-  params: Input[`${M} ${P}`]
-) => Promise<Response[`${M} ${P}`]>;
+export type Provider = <M extends Method, P extends Path>(method: M, path: P, params: Input[`${M} ${P}`]) => Promise<Response[`${M} ${P}`]>
 
-export type Implementation = (
-  method: Method,
-  path: string,
-  params: Record<string, any>
-) => Promise<any>;
+export type Implementation = (method: Method, path: string, params: Record<string, any>) => Promise<any>
 
 export class ExpressZodAPIClient {
-  constructor(protected readonly implementation: Implementation) {}
-  public readonly provide: Provider = async (method, path, params) =>
-    this.implementation(
-      method,
-      Object.keys(params).reduce(
-        (acc, key) => acc.replace(`:${key}`, params[key]),
-        path
-      ),
-      Object.keys(params).reduce(
-        (acc, key) =>
-          path.indexOf(`:${key}`) >= 0 ? acc : { ...acc, [key]: params[key] },
-        {}
-      )
-    );
+    constructor(protected readonly implementation: Implementation) { }
+    public readonly provide: Provider = async (method, path, params) => this.implementation(method, Object.keys(params).reduce((acc, key) => acc.replace(`:${key}`, params[key]), path), Object.keys(params).reduce((acc, key) => path.indexOf(`:${key}`) >= 0 ? acc : { ...acc, [key]: params[key] }, {}));
 }
