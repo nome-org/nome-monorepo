ec142dd6557eec858664574e8fb596b63ea923bb
hz <13457698+hozzjss@users.noreply.github.com>
2024-01-11 17:00:30 +0200

    fix ui bug for rarity selection in smaller screens

--- apps/gif-creator-ui/src/components/shared/SelectRarity.vue
+++ apps/gif-creator-ui/src/components/shared/SelectRarity.vue
@@ -11,7 +11,7 @@ const { selectedRarity } = defineProps({
 defineEmits(["update:selectedRarity"]);
 </script>
 <template>
-  <div class="grid md:grid-flow-row grid-cols-3 gap-x-4 gap-y-5 w-full">
+  <div class="grid md:grid-flow-row grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-5 w-full">
     <button v-for="rarity in available_rarity" :key="rarity" @click="$emit('update:selectedRarity', rarity)"
       :class="rarity == selectedRarity ? 'bg-gray-500' : 'bg-transparent'"
       class="border-solid border border-white cursor-pointer uppercase md:h-8 h-12 transition-all text-white rounded-xl w-[10rem]">
