5be691d13949d40e938f10825af38d3a70fe37ac
hz <13457698+hozzjss@users.noreply.github.com>
2024-01-16 22:39:54 +0200

    fix responsive design for intro page

--- apps/gif-creator-ui/src/pages/Intro.vue
+++ apps/gif-creator-ui/src/pages/Intro.vue
@@ -25,26 +25,26 @@ const isVerified = computed(() => auth.isLoggedIn)
         <div class="text-pink">
           BUY $N0ME BRC-20 TO
         </div>
-        <Button :disabled="isVerified" @click="login" class-name="mt-4" type="button" class="{{ btnClass }}">
+        <Button :disabled="isVerified" @click="login" class-name="mt-4">
           VERIFY
         </Button>
       </div>
-      <div class="flex flex-col sm:flex-row w-full max-w-3xl mt-20 sm:h-32">
-        <router-link to="/gallery" class="relative bottom-2 right-4 self-end">
-          <Button :disabled="!isVerified" type="button" class-name="relative bottom-2 right-4 self-end">
+      <div class="flex flex-col gap-y-8 items-center sm:flex-row sm:w-full max-w-3xl mt-20 sm:h-32">
+        <router-link to="/gallery" class="sm:relative right-4 sm:self-end">
+          <Button :disabled="!isVerified">
             Expo
           </Button>
         </router-link>
-        <div class="flex gap-x-20 w-full text-xs">
-          <div class="w-5/12 rounded-br-md border-r border-b border-gray-600 h-5/6">
-            <div class="relative bottom-12 right-12">
+        <div class="gap-x-20 w-full text-xs flex flex-col items-center gap-y-8 sm:flex-row">
+          <div class="w-1/2 sm:w-5/12 rounded-br-md sm:border-r sm:border-b border-gray-600 h-5/6">
+            <div class="sm:relative bottom-12 right-12">
               The first exhibition BEAR MARKET is a manifestation of a bull run. It is an extraordinary
               digital
               immersive exhibition with sound with a free 1/1 art inscription to mint.
             </div>
           </div>
-          <div class="w-5/12 rounded-tl-md border-l border-t border-gray-600 h-5/6">
-            <div class="relative top-12 left-12">
+          <div class="w-1/2 sm:w-5/12 rounded-tl-md sm:border-l sm:border-t border-gray-600 h-5/6">
+            <div class="sm:relative top-12 left-12">
 
               This is a platform for the community to explore the potential of Bitcoin Ordinals. Create GIF
               recursive animations on-chain and inscribe on rare sats all in one place.
@@ -52,8 +52,8 @@ const isVerified = computed(() => auth.isLoggedIn)
 
           </div>
         </div>
-        <router-link to="/gif">
-          <Button :disabled="!isVerified" type="button" class-name="relative bottom-4 left-4 self-start">
+        <router-link to="/gif" class="sm:relative left-4 sm:self-start">
+          <Button :disabled="!isVerified">
             Tool
           </Button>
         </router-link>
