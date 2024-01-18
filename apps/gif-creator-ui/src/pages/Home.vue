6622d9cf0960cec7d01ca91abecd9bff0ef693c3
hz <13457698+hozzjss@users.noreply.github.com>
2024-01-18 17:03:08 +0200

    fix remove enter button and restore nome logo fix remove global font sizes

--- apps/gif-creator-ui/src/pages/Home.vue
+++ apps/gif-creator-ui/src/pages/Home.vue
@@ -11,12 +11,15 @@
           class="uppercase text-white text-4xl text-center flex justify-between w-3/4 max-w-[24rem] sm:w-full mx-auto mb-6">
           <span v-for="item in 'Gallery'.split('')">{{ item }}</span>
         </h1>
-        <router-link type="button" to="/intro"
-          class="uppercase bg-white text-black tracking-[0.3em] py-1 px-4 rounded-md transition-all hover:scale-110">
+        <!-- TODO: original show image, on hover show the button -->
+        <!-- DONE: remove enter button for now so we can test on /intro -->
+        <!-- TODO: button text is transparent -->
+        <!-- <router-link type="button" to="/intro"
+          class="uppercase bg-none text-black mix-blend-screen tracking-[0.3em] py-1 px-4 rounded-md transition-all hover:scale-110 before:absolute before:top-0 before:right-0 before:bottom-0 before:left-0 before:rounded-md before:mix-blend-color-burn">
           enter
-        </router-link>
-        <!-- <img src="../assets/images/logo-white.png" alt=""
-          class="w-3/4 max-w-[24rem] sm:w-full mx-auto mt-8 hover:opacity-0 transition-all ease-in" /> -->
+        </router-link> -->
+        <img src="../assets/images/logo-white.png" alt=""
+          class="w-3/4 max-w-[24rem] sm:w-full mx-auto mt-8 hover:opacity-0 transition-all ease-in" />
       </div>
       <div class="fixed bottom-0 w-full flex justify-between pb-10 text-white sm:text-xl px-4 sm:px-12 flex-wrap">
         <div class="capitalize">JAN 2024</div>
