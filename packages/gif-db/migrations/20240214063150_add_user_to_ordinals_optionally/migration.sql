2c8c6de80c6a91a3204c65c4ad0a80661165d94d
hz <gravatar.m2a16@simplelogin.com>
2024-02-14 08:37:41 +0200

    fix remove user from ordinal for now

--- packages/gif-db/migrations/20240214063150_add_user_to_ordinals_optionally/migration.sql
+++ packages/gif-db/migrations/20240214063150_add_user_to_ordinals_optionally/migration.sql
@@ -6,6 +6,3 @@
 */
 -- AlterTable
 ALTER TABLE `Ordinal` MODIFY `receiver_address` VARCHAR(255) NOT NULL;
-
--- AddForeignKey
-ALTER TABLE `Ordinal` ADD CONSTRAINT `Ordinal_receiver_address_fkey` FOREIGN KEY (`receiver_address`) REFERENCES `User`(`ordinal_address`) ON DELETE RESTRICT ON UPDATE CASCADE;
